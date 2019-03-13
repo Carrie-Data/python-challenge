ef46cf4d8e64ced845bcbf6868877843f94ea81c
Carrie Cook <carriecook82@gmail.com>
2019-03-13 14:28:45 -0600

    complete excerise and formated output

--- PyBank/main.py
+++ PyBank/main.py
@@ -16,8 +16,8 @@ with open(BudgetCSV, 'r') as csvfile:
 
     total_profit = 0
     total_change = 0
-    min_profit = int(data[0][1]) -int(data[1][1])
-    max_profit = int(data[0][1]) -int(data[1][1])
+    min_profit = int(data[1][1]) -int(data[0][1])
+    max_profit = int(data[1][1]) -int(data[0][1])
 
     for row in data:
         total_profit += int(row[1]) 
@@ -37,15 +37,16 @@ with open(BudgetCSV, 'r') as csvfile:
             month_max_profit = data[i + 1][0]
 
     average = total_change/(months - 1)
+    average = ("{0:.2f}".format(average))
   
     #print calculated values
     print("Financial Analysis")
     print("-------------------------")
     print(f"Total Months: {months}")
-    print(f"Total: {total_profit}")
-    print(f"Average Change: {average}")
-    print(f"Greatest Increase in Profits: {month_max_profit} {max_profit}")
-    print(f"Greatest Decrease in Profits: {month_min_profit} {min_profit}")
+    print(f"Total: ${total_profit}")
+    print(f"Average Change: ${average}")
+    print("Greatest Increase in Profits: " + month_max_profit + "  ($" + str(max_profit) + ")")
+    print("Greatest Decrease in Profits: " + month_min_profit + "  ($" + str(min_profit) + ")")
     print("-----")
 
 #create text file and write results to it.
@@ -54,13 +55,13 @@ output_file = os.path.join("Financial_Analysis.txt")
 
 #  Open the output file
 with open(output_file, "w", newline="") as datafile:
-    datafile.write("Financial Analysis",)
-    datafile.write("-------------------------",)
-    datafile.write(f"Total Months: {months}",)
-    datafile.write(f"Total: {total_profit}")
-    datafile.write(f"Average Change: {average}")
-    datafile.write(f"Greatest Increase in Profits: {month_max_profit} {max_profit}")
-    datafile.write(f"Greatest Decrease in Profits: {month_min_profit} {min_profit}")
+    datafile.write("Financial Analysis\n",)
+    datafile.write("-------------------------\n")
+    datafile.write(f"Total Months: {months}\n")
+    datafile.write(f"Total: ${total_profit}\n")
+    datafile.write(f"Average Change: ${average}\n")
+    datafile.write("Greatest Increase in Profits: " + month_max_profit + "  ($" + str(max_profit) + ")\n")
+    datafile.write("Greatest Decrease in Profits: " + month_min_profit + "  ($" + str(min_profit) + ")\n")
     datafile.write("-----")
 
     datafile.close()
